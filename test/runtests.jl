aa8ff6cdf7bdf44e43a3c6c545143dc9b6509b71
Rufat Badal <tours-flashy-0j@icloud.com>
2023-01-29 14:47:14 +0100

    test for scalar * M

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using TVESim
 using Test
+using Random
+using JuMP
 
-@testset "TVESim.jl" begin
-    # Write your tests here.
+const RNG_SEED = 42
+
+@testset "Linear algebra operations" begin
+    rng = MersenneTwister(RNG_SEED)
+    n = 100
+    model = Model()
+
+    A_val = rand(rng, n, n)
+    @NLexpression(model, A[i=1:n, j=1:n], A_val[i, j])
+
+    @test 2A_val ≈ value.(2A)
 end
