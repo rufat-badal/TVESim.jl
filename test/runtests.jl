ee362df161d87f58a72ea8c72c1cba5985e8422d
Rufat Badal <tours-flashy-0j@icloud.com>
2023-01-29 15:38:49 +0100

    refactored A, A_val generation

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,26 +5,28 @@ using JuMP
 
 const RNG_SEED = 42
 
+function get_matrix_pair(rng, model, m, n)
+    A_val = rand(rng, m, n)
+    A = @NLexpression(model, [i = 1:m, j = 1:n], A_val[i, j])
+    A, A_val
+end
+
 @testset "Linear algebra operations" begin
     rng = MersenneTwister(RNG_SEED)
     model = Model()
 
     m, n = 100, 120
     scalar = 23.0
-    A_val = rand(rng, m, n)
-    A = @NLexpression(model, [i = 1:m, j = 1:n], A_val[i, j])
+    A, A_val = get_matrix_pair(rng, model, m, n)
     @test value.(scalar * A) ≈ scalar * A_val
     @test value.(-A) ≈ -A_val
 
-    B_val = rand(rng, m, n)
-    B = @NLexpression(model, [i = 1:m, j = 1:n], B_val[i, j])
+    B, B_val = get_matrix_pair(rng, model, m, n)
     @test value.(A + B) ≈ A_val + B_val
     @test value.(A - B) ≈ A_val - B_val
 
     m, n, l = 100, 123, 78
-    A_val = rand(rng, m, n)
-    A = @NLexpression(model, [i = 1:m, j = 1:n], A_val[i, j])
-    B_val = rand(rng, n, l)
-    B = @NLexpression(model, [i = 1:n, j = 1:l], B_val[i, j])
+    A, A_val = get_matrix_pair(rng, model, m, n)
+    B, B_val = get_matrix_pair(rng, model, n, l)
     @test value.(A * B) ≈ A_val * B_val
 end
