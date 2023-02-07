fd4dc83a933cfcb0919175e9d1c5961b6720bbb3
Rufat Badal <tours-flashy-0j@icloud.com>
2023-02-07 18:19:15 +0100

    determine if vertex is internal/external/boundary

--- src/grid.jl
+++ src/grid.jl
@@ -8,11 +8,7 @@ mutable struct Vertex
     Vertex(x, y, type=undetermined) = new(x, y, type)
 end
 
-struct Triangle
-    a::Vertex
-    b::Vertex
-    c::Vertex
-end
+Triangle = Tuple{Vertex,Vertex,Vertex}
 
 function isosceles_right_triangulation(width::Float64, height::Float64)::Vector{Triangle}
     num_squares_hor = ceil(Int, width)
@@ -31,8 +27,8 @@ function isosceles_right_triangulation(width::Float64, height::Float64)::Vector{
     k = 1
     for i in 1:num_squares_hor
         for j in 1:num_squares_ver
-            triangles[k] = Triangle(vertices[i, j], vertices[i+1, j], vertices[i+1, j+1])
-            triangles[k+1] = Triangle(vertices[i, j], vertices[i, j+1], vertices[i+1, j+1])
+            triangles[k] = (vertices[i, j], vertices[i+1, j], vertices[i+1, j+1])
+            triangles[k+1] = (vertices[i, j], vertices[i, j+1], vertices[i+1, j+1])
             k += 2
         end
     end
@@ -40,6 +36,48 @@ function isosceles_right_triangulation(width::Float64, height::Float64)::Vector{
     triangles
 end
 
-for T in isosceles_right_triangulation(2.3, 3.1)
-    println(T)
+struct SimulationGrid
+    num_vertices::Int
+    num_dirichlet_vertices::Int
+    num_neumann_vertices::Int
+    x::Vector{Float64}
+    y::Vector{Float64}
+    triangles::Vector{Tuple{Int,Int,Int}}
 end
+
+function SimulationGrid(width, height, triangulation, isinternal, isdirichlet)
+    covering_triangulation = triangulation(width, height)
+    vertex_isinternal(v::Vertex) = isinternal(v.x, v.y)
+    triangle_isinternal(triangle::Triangle) = all(vertex_isinternal(v) for v in triangle)
+    # on the first pass determine if a vertex is internal, external, or on the boundary
+    for T in covering_triangulation
+        containing_triangle_isinternal = triangle_isinternal(T)
+        for v in T
+            update_vertex_type(v, containing_triangle_isinternal)
+        end
+    end
+end
+
+function update_vertex_type(v::Vertex, containing_triangle_isinternal::Bool)
+    if v.type == boundary
+        return
+    end
+
+    if v.type == undetermined
+        v.type = containing_triangle_isinternal ? internal : external
+        return
+    end
+
+    if (v.type == external && containing_triangle_isinternal) || (v.type == internal && !containing_triangle_isinternal)
+        v.type = boundary
+    end
+end
+
+radius = 2.0
+dirichlet_arc_angle = pi / 4
+width = 2 * radius
+height = 2 * radius
+triangulation = isosceles_right_triangulation
+isinternal(x, y) = (x - radius)^2 + (y - radius)^2 <= radius^2
+isdirichlet(x, y) = x - radius <= -cos(dirichlet_arc_angle)
+SimulationGrid(width, height, triangulation, isinternal, isdirichlet)
