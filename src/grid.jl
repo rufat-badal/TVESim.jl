8548930f260a65153c867a11e3c2a48698313df4
Rufat Badal <tours-flashy-0j@icloud.com>
2023-02-14 21:24:33 +0100

    plot equilateral mesh

--- src/grid.jl
+++ src/grid.jl
@@ -2,6 +2,8 @@ import CairoMakie
 
 @enum VertexType external boundary internal undetermined
 
+const triangle_side_length = 1.0
+
 mutable struct Vertex
     x::Float64
     y::Float64
@@ -12,9 +14,9 @@ end
 
 Triangle = Tuple{Vertex,Vertex,Vertex}
 
-function isosceles_right_triangulation(width::Float64, height::Float64)
-    num_squares_hor = ceil(Int, width)
-    num_squares_ver = ceil(Int, height)
+function isosceles_right_triangulation(width::Number, height::Number)
+    num_squares_hor = ceil(Int, width / triangle_side_length)
+    num_squares_ver = ceil(Int, height / triangle_side_length)
     num_vertices_hor = num_squares_hor + 1
     num_vertices_ver = num_squares_ver + 1
     vertices = Matrix{Vertex}(undef, num_vertices_hor, num_vertices_ver)
@@ -31,6 +33,37 @@ function isosceles_right_triangulation(width::Float64, height::Float64)
         for j in 1:num_squares_ver
             triangles[k] = (vertices[i, j], vertices[i+1, j], vertices[i+1, j+1])
             triangles[k+1] = (vertices[i, j], vertices[i, j+1], vertices[i+1, j+1])
+            k += 2
+        end
+    end
+
+    triangles
+end
+
+function equilateral_triangulation(width::Number, height::Number)
+    triangle_height = sqrt(3) / 2 * triangle_side_length
+    num_layers = ceil(Int, height / triangle_height)
+    num_upward_triangles_per_layer = ceil(Int, (width - triangle_side_length / 2) / triangle_side_length) + 1
+    vertices = Matrix{Vertex}(undef, num_upward_triangles_per_layer + 1, num_layers + 1)
+    for i in 1:(num_upward_triangles_per_layer+1)
+        for j in 1:(num_layers+1)
+            vertices[i, j] = Vertex(-(j % 2) * triangle_side_length / 2 + (i - 1) * triangle_side_length, (j - 1) * triangle_height)
+        end
+    end
+
+    num_triangles = 2 * num_upward_triangles_per_layer * num_layers
+    triangles = Vector{Triangle}(undef, num_triangles)
+    k = 1
+    for j in 1:num_layers
+        for i in 1:num_upward_triangles_per_layer
+            if j % 2 == 0
+                triangles[k] = (vertices[i, j], vertices[i+1, j+1], vertices[i, j+1])
+                triangles[k+1] = (vertices[i, j], vertices[i+1, j], vertices[i+1, j+1])
+            else
+                triangles[k] = (vertices[i, j], vertices[i+1, j], vertices[i, j+1])
+                triangles[k+1] = (vertices[i+1, j], vertices[i+1, j+1], vertices[i, j+1])
+            end
+
             k += 2
         end
     end
@@ -118,7 +151,7 @@ function update_vertex_type(v::Vertex, containing_triangle_isinternal::Bool)
     end
 end
 
-function plot(grid::SimulationGrid, temp_range)
+function plot(grid::SimulationGrid, temp_range; show_edges=false)
     plot_width = 1000 # in pixels!
     fontsize = 20
     min_x, max_x = minimum(grid.x), maximum(grid.x)
@@ -140,18 +173,26 @@ function plot(grid::SimulationGrid, temp_range)
         end
     end
 
-    CairoMakie.mesh!(
-        vertices, faces, color=grid.θ,
-        colormap=:plasma, colorrange=temp_range, shading=false)
+    if show_edges
+        CairoMakie.poly!(
+            vertices, faces,
+            color=grid.θ, colormap=:plasma, colorrange=temp_range, strokewidth=1, shading=true)
+    else
+        CairoMakie.mesh!(
+            vertices, faces, color=grid.θ,
+            colormap=:plasma, colorrange=temp_range)
+    end
+
     fig
 end
 
-radius = 20.0
+
+radius = 10
 dirichlet_arc_angle = pi / 4
 width = 2 * radius
 height = 2 * radius
-triangulation = isosceles_right_triangulation
+triangulation = equilateral_triangulation
 isinternal(x, y) = (x - radius)^2 + (y - radius)^2 <= radius^2
 isdirichlet(x, y) = x - radius <= -cos(dirichlet_arc_angle)
 grid = SimulationGrid(width, height, 0.0, triangulation, isinternal, isdirichlet)
-plot(grid, (0.0, 1.0))
+plot(grid, (0.0, 1.0), show_edges=true)
