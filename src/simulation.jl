2384e985dd9fde4b28bd11020699461aa802617d
Rufat Badal <tours-flashy-0j@icloud.com>
2023-02-22 12:08:30 +0100

    created objective for the mechanical step

--- src/simulation.jl
+++ src/simulation.jl
@@ -4,31 +4,16 @@ struct SimulationStep
     θ::Vector{Float64}
 end
 
-Base.@kwdef struct Simulation
-    grid::SimulationGrid
-    fps::Number = 30
-    initial_temperature::Number = 0.0
-    initial_scaling::Number = 1.0
-    shape_memory_scaling::Number
-    temperature_search_radius::Number
-    deformation_search_radius::Number
-    heat_conductivity::Vector{Number} = [1.0, 1.0]
-    heat_transfer_coefficient::Number = 0.5
-    entropic_heat_capacity::Number = 10.0
-    external_temperature::Number = 0.0
-    mechanical_step::JuMP.Model # TODO: create custom struct
-    thermal_step::JuMP.Model # TODO: create custom struct
-    steps::Vector{SimulationStep}
+struct MechanicalStep
+    model::JuMP.Model
+    prev_x::Vector{JuMP.NonlinearParameter}
+    prev_y::Vector{JuMP.NonlinearParameter}
+    prev_θ::Vector{JuMP.NonlinearParameter}
+    x::Vector{JuMP.VariableRef}
+    y::Vector{JuMP.VariableRef}
 end
 
-function Simulation(grid::SimulationGrid, deformation_search_radius, temperature_search_radius, shape_memory_scaling=2.0)
-    mechanical_step = create_mechanical_step(grid, deformation_search_radius, shape_memory_scaling)
-    thermal_step = create_thermal_step()
-    steps = Vector{SimulationStep}()
-    Simulation(; grid, mechanical_step, thermal_step, steps, deformation_search_radius, temperature_search_radius, shape_memory_scaling)
-end
-
-function create_mechanical_step(grid::SimulationGrid, search_rad, shape_memory_scaling)
+function MechanicalStep(grid::SimulationGrid, search_rad)
     m = JuMP.Model(() -> MadNLP.Optimizer(print_level=MadNLP.WARN, blas_num_threads=8))
 
     # previous steps
@@ -51,6 +36,40 @@ function create_mechanical_step(grid::SimulationGrid, search_rad, shape_memory_s
     )
     JuMP.@constraint(m, fix_y[i=1:grid.num_dirichlet_vertices], y[i] == grid.y[i])
 
+    MechanicalStep(m, prev_x, prev_y, prev_θ, x, y)
+end
+
+Base.@kwdef struct Simulation
+    grid::SimulationGrid
+    deformation_search_radius::Number
+    temperature_search_radius::Number
+    fps::Number = 30
+    initial_temperature::Number = 0.0
+    initial_scaling::Number = 1.0
+    shape_memory_scaling::Number = 2.0
+    heat_conductivity::Vector{Number} = [1.0, 1.0]
+    heat_transfer_coefficient::Number = 0.5
+    entropic_heat_capacity::Number = 10.0
+    external_temperature::Number = 0.0
+    mechanical_step::MechanicalStep
+    steps::Vector{SimulationStep} = Vector{SimulationStep}()
+end
+
+function Simulation(grid::SimulationGrid, deformation_search_radius, temperature_search_radius)
+    mechanical_step = MechanicalStep(grid, deformation_search_radius)
+    simulation = Simulation(; grid, deformation_search_radius, temperature_search_radius, mechanical_step)
+    create_mechanical_step_objective(simulation)
+end
+
+function create_mechanical_step_objective(simulation::Simulation)
+    m = simulation.mechanical_step.model
+    prev_x = simulation.mechanical_step.prev_x
+    prev_y = simulation.mechanical_step.prev_y
+    prev_θ = simulation.mechanical_step.prev_θ
+    x = simulation.mechanical_step.x
+    y = simulation.mechanical_step.y
+    grid = simulation.grid
+
     # compute strains and symmetrized strain-rates
     prev_triangles = [[NLExprVector(m, [prev_x[i], prev_y[i]]) for i in T] for T in grid.triangles]
     triangles = [[NLExprVector(m, [x[i], y[i]]) for i in T] for T in grid.triangles]
@@ -72,7 +91,7 @@ function create_mechanical_step(grid::SimulationGrid, search_rad, shape_memory_s
     end
 
     # objective
-    scaling_matrix = [1/shape_memory_scaling 0; 0 1]
+    scaling_matrix = [1/simulation.shape_memory_scaling 0; 0 1]
     scaled_strains = [F * scaling_matrix for F in strains]
     JuMP.@NLexpression(
         m, elastic_energy,
@@ -86,8 +105,8 @@ function create_mechanical_step(grid::SimulationGrid, search_rad, shape_memory_s
             )
         )
     )
-
-    m
+    JuMP.@NLexpression(m, dissipation, 0.5 * sum(d for d in norm_sqr.(symmetrized_strain_rates)))
+    JuMP.@NLobjective(m, Min, elastic_energy + simulation.fps * dissipation)
 end
 
 function neo_hook(F::NLExprMatrix)
@@ -100,7 +119,7 @@ function austenite_percentage(θ)
     return 1 - 1 / (1 + θ)
 end
 
-function strain(triangle, reference_triangle)
+function strain((triangle, reference_triangle))
     a, b, c = reference_triangle
     gradient_equilateral_to_reference = [b - a c - a]
     a, b, c = triangle
@@ -109,11 +128,5 @@ function strain(triangle, reference_triangle)
     gradient_equilateral_to_current * inv(gradient_equilateral_to_reference)
 end
 
-function strain((triangle, reference_triangle))
-    strain(triangle, reference_triangle)
-end
-
-function create_thermal_step()
-    m = JuMP.Model(() -> MadNLP.Optimizer(print_level=MadNLP.WARN, blas_num_threads=4))
-    m
+function init_thermal_step()
 end
