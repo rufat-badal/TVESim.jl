ac3e40c10b320284981780ec1116b97fa922cd3a
Rufat Badal <tours-flashy-0j@icloud.com>
2023-03-03 17:26:21 +0100

    create_objective for mechanical step works again

--- src/simulation.jl
+++ src/simulation.jl
@@ -286,26 +286,27 @@ function create_objective!(
     # compute strains and symmetrized strain-rates
     prev_strains, strains = get_strains(m, prev_x, prev_y, x, y, grid)
     symmetrized_strain_rates = get_symmetrized_strain_rates(prev_strains, strains)
-    # JuMP.register(m, :austenite_percentage, 1, austenite_percentage; autodiff=true)
-    # austenite_percentages = integral(austenite_percentage, prev_θ, grid.triangles, m)
+    JuMP.register(m, :austenite_percentage, 1, austenite_percentage; autodiff=true)
+    austenite_percentages = integral(austenite_percentage, prev_θ, grid.triangles, m)
 
     # objective
-    # scaling_matrix = [1/shape_memory_scaling 0; 0 1]
-    # scaled_strains = [F * scaling_matrix for F in strains]
-    # JuMP.@NLexpression(
-    #     m, elastic_energy,
-    #     0.5 * sum(
-    #         a_perc * neo_hook_F + (1 - a_perc) * neo_hook_F_scaled
-    #         for (F, a_perc, neo_hook_F, neo_hook_F_scaled) in zip(
-    #             strains,
-    #             austenite_percentages,
-    #             neo_hook.(strains),
-    #             neo_hook.(scaled_strains)
-    #         )
-    #     )
-    # )
-    # JuMP.@NLexpression(m, dissipation, 0.5 * sum(d for d in norm_sqr.(symmetrized_strain_rates)))
-    # JuMP.@NLobjective(m, Min, elastic_energy + fps * dissipation)
+    scaling_matrix = [1/shape_memory_scaling 0; 0 1]
+    scaled_strains = [F * scaling_matrix for F in strains]
+
+    JuMP.@NLexpression(
+        m, elastic_energy,
+        0.5 * sum(
+            a_perc * neo_hook_F + (1 - a_perc) * neo_hook_F_scaled
+            for (F, a_perc, neo_hook_F, neo_hook_F_scaled) in zip(
+                strains,
+                austenite_percentages,
+                neo_hook.(strains),
+                neo_hook.(scaled_strains)
+            )
+        )
+    )
+    JuMP.@NLexpression(m, dissipation, 0.5 * sum(d.expression for d in norm_sqr.(symmetrized_strain_rates)))
+    JuMP.@NLobjective(m, Min, elastic_energy + fps * dissipation)
 end
 
 function strain((triangle, reference_triangle))
--- src/special_functions.jl
+++ src/special_functions.jl
@@ -1,12 +1,13 @@
-# function austenite_percentage(θ)
-#     return 1 - 1 / (1 + θ)
-# end
+function austenite_percentage(θ)
+    return 1 - 1 / (1 + θ)
+end
 
-# function neo_hook(F::NLExprMatrix)
-#     trace_C = tr(transpose(F) * F)
-#     det_F = det(F)
-#     JuMP.@NLexpression(F.model, trace_C - 2 - 2 * log(det_F) + (det_F - 1)^2)
-# end
+function neo_hook(F::Matrix{AdvancedNonlinearExpression})
+    model = F[1, 1].model
+    trace_C = tr(transpose(F) * F).expression
+    det_F = det(F).expression
+    JuMP.@NLexpression(model, trace_C - 2 - 2 * log(det_F) + (det_F - 1)^2)
+end
 
 # function gradient_austenite_potential(F::NLExprMatrix)
 #     det_F = det(F)
