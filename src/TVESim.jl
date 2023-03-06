bb08f3d3d919eb00517832fe49434497f1cbeebd
Rufat Badal <tours-flashy-0j@icloud.com>
2023-03-06 11:43:31 +0100

    fixed wrong include

--- src/TVESim.jl
+++ src/TVESim.jl
@@ -12,7 +12,7 @@ import CairoMakie
 import Triangulate
 import ProgressBars
 
-include("linalg_for_nlexpr_with_symbols.jl")
+include("linalg_for_nlexpr.jl")
 include("grid.jl")
 include("special_functions.jl")
 include("simulation.jl")
