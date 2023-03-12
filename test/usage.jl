0e81df841945803f39be19dc9c4b0f6896062dd3
Rufat Badal <tours-flashy-0j@icloud.com>
2023-03-12 17:56:53 +0100

    added cooling square experiment

--- test/usage.jl
+++ test/usage.jl
@@ -19,7 +19,25 @@ function square_experiment()
     num_steps = 40
     simulate!(simulation, num_steps - 2)
     for i in 1:num_steps
-        save("$experiments_directory/square_step_$i.png", TVESim.plot(simulation, i, show_edges=true))
+        save("$experiments_directory/step_$i.png", TVESim.plot(simulation, i, show_edges=true))
+    end
+    save("$experiments_directory/grid.png", TVESim.plot(grid, simulation.θ_range, show_edges=true))
+    return
+end
+
+function cooling_square_experiment()
+    experiments_directory = setup_experiment_directory("cooling_square")
+    width = 10
+    height = 10
+    initial_temperature = 10
+    isinternal(x, y) = -(width + 1) / 2 <= x <= (width + 1) / 2 && -(height + 1) / 2 <= y <= (height + 1) / 2
+    isdirichlet(x, y) = x <= -(width - 1) / 2
+    grid = SimulationGrid(width + 2, height + 2, initial_temperature, TVESim.isosceles_right_triangulation, isinternal, isdirichlet)
+    simulation = Simulation(grid, fps=1.5, heat_transfer_coefficient=4)
+    num_steps = 50
+    simulate!(simulation, num_steps - 2)
+    for i in 1:num_steps
+        save("$experiments_directory/step_$i.png", TVESim.plot(simulation, i, show_edges=true))
     end
     save("$experiments_directory/grid.png", TVESim.plot(grid, simulation.θ_range, show_edges=true))
     return
@@ -45,3 +63,4 @@ end
 
 circle_experiment()
 square_experiment()
+cooling_square_experiment()
